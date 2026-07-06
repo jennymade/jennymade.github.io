81302c62352456f28aa1fe346f11acc2bd3c4364
Jenny Kwon <jennymade@proton.me>
2026-07-06 12:53:52 -0700

    uploading updated product and brand designer and developer cat card designs

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,25 +2,33 @@ import React from "react";
 
 type CardProps = {
   role?: string;
+  roleImg?: string;
+  roleImgAlt?: string;
+  roleClass?: string;
   desc?: string;
-}
+};
 
 export class Card extends React.Component<CardProps> {
   render() {
     return (
       <div className="cardContainer">
         <div className="card" tabIndex={0}>
-          <span className="front">
+          <div className="front">
+            <img
+              className={this.props.roleClass}
+              src={this.props.roleImg}
+              alt={this.props.roleImgAlt}
+            ></img>
             <p className="cardRole">{this.props.role}</p>
-            <p className="flipInstruct">Hover to flip card</p>
-          </span>
-          <span className="back">
+            <span className="flipInstruct">hover to flip card</span>
+          </div>
+          <div className="back">
             <ul>
               <li>{this.props.desc}</li>
             </ul>
-          </span>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
--- src/components/card/cardSection.tsx
+++ src/components/card/cardSection.tsx
@@ -7,14 +7,23 @@ export class CardSection extends React.Component {
       <div className="cardWrapper">
         <Card
           role="Product and brand designer"
+          roleImg="src/components/card/greenCatDesigner.png"
+          roleImgAlt="A green Cheshire-inspired cat wearing glasses holding a sharpie and facing post-it notes, with a mobile phone mockup above it"
+          roleClass="designCat"
           desc="Loves bridging the designer-engineer gap — skilled at ideating on Figma"
         />
         <Card
           role="Developer"
+          roleImg="src/components/card/greenCatDeveloper.png"
+          roleImgAlt="A green Cheshire-inspired cat wearing glasses in front of a laptop with code against black window screens above"
+          roleClass="developerCat"
           desc="Loves bridging the designer-engineer gap. Works primarily in React and TypeScript, getting the hang of Svelte."
         />
         <Card
           role="Data journalist, analyst and researcher"
+          roleImg="src/components/card/greenCatResearch.png"
+          roleImgAlt="A green Cheshire-inspired cat wearing glasses in front of a laptop"
+          roleClass="researchCat"
           desc="Avid lover of searching for and scraping data from hard-to-get places. Experienced at working with unstructured and large structured data using Python and occasionally R and SQL."
         />
       </div>
